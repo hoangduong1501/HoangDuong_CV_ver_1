215ec86f11d229131b934ed0bb4d3bbef00a451d
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2021-04-28 21:10:31 +0700

    .

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,20 @@
-// $(document).ready(function(){
-//     var width = $(window).width();
-//     console.log(width);
-//     if (width < 540){
-//         // $('.left, .right').addClass('responsive_768');
-//         alert('nho hon 540');
-//     }
-//     else{
-//         // $('.left, .right').removeClass('responsive_768');
-//         alert('lon hon hoac bang 540');
-//     }
-// });
+$(document).ready(function(){
+    // var width = $(window).width();
+    // console.log(width);
+    // if (width < 540){
+    //     // $('.left, .right').addClass('responsive_768');
+    //     alert('nho hon 540');
+    // }
+    // else{
+    //     // $('.left, .right').removeClass('responsive_768');
+    //     alert('lon hon hoac bang 540');
+    // }
+    $.ajax({url: "https://hoangduong1501.github.io/HoangDuong_CV_ver_1/Infor_Tab.html", 
+        success: function(result){
+            $("#div1").html(result);
+        }
+    });
+});
 
 // $(window).resize(function(){ 
 //     var width = $(window).width();
