c620f6e9bb150e93515ed35d087fcc501a5fc494
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2021-04-28 20:51:44 +0700

    .

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-    var width = $(window).width();
-    console.log(width);
-    if (width < 540){
-        // $('.left, .right').addClass('responsive_768');
-        alert('nho hon 540');
-    }
-    else{
-        // $('.left, .right').removeClass('responsive_768');
-        alert('lon hon hoac bang 540');
-    }
-});
+// $(document).ready(function(){
+//     var width = $(window).width();
+//     console.log(width);
+//     if (width < 540){
+//         // $('.left, .right').addClass('responsive_768');
+//         alert('nho hon 540');
+//     }
+//     else{
+//         // $('.left, .right').removeClass('responsive_768');
+//         alert('lon hon hoac bang 540');
+//     }
+// });
 
-$(window).resize(function(){ 
-    var width = $(window).width();
-    console.log(width);
-    if (width < 540){
-        // $('.left, .right').addClass('responsive_768');
-        alert('nho hon 540');
-    }
-    else{
-        // $('.left, .right').removeClass('responsive_768');
-        alert('lon hon hoac bang 540');
-    }
-});
+// $(window).resize(function(){ 
+//     var width = $(window).width();
+//     console.log(width);
+//     if (width < 540){
+//         // $('.left, .right').addClass('responsive_768');
+//         alert('nho hon 540');
+//     }
+//     else{
+//         // $('.left, .right').removeClass('responsive_768');
+//         alert('lon hon hoac bang 540');
+//     }
+// });
