ab5e7518986ca28195635b087741152affe77ce9
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2021-04-28 21:56:51 +0700

    .

--- JS/script.js
+++ JS/script.js
@@ -16,6 +16,37 @@ $(document).ready(function(){
     });
 });
 
+$('#btn_ThongTin').click(function(){
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+    $.ajax({url: "https://hoangduong1501.github.io/HoangDuong_CV_ver_1/Infor_Tab.html", 
+        success: function(result){
+            $("#div1").html(result);
+        }
+    });
+});
+
+$('#btn_KinhNghiem').click(function(){
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+    $.ajax({url: "https://hoangduong1501.github.io/HoangDuong_CV_ver_1/Exp_Tab.html", 
+        success: function(result){
+            $("#div1").html(result);
+        }
+    });
+});
+
+$('#btn_KyNang').click(function(){
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+    $.ajax({url: "https://hoangduong1501.github.io/HoangDuong_CV_ver_1/Infor_Tab.html", 
+        success: function(result){
+            $("#div1").html(result);
+        }
+    });
+});
+
+
 // $(window).resize(function(){ 
 //     var width = $(window).width();
 //     console.log(width);
